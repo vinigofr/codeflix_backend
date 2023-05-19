4a21c8fce3ad78f3651ec7407e0317b0dbe087db
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2023-05-19 03:22:57 +0000

    Adjust deepFreeze method

--- src/@seedwork/domain/utils/object.ts
+++ src/@seedwork/domain/utils/object.ts
@@ -1,5 +1,5 @@
 export function deepFreeze<T>(obj: T) {
-  const propNames = Object.getOwnPropertyNames(obj);
+  const propNames = Object.getOwnPropertyNames(obj || {});
 
   for (const name of propNames) {
     const value = obj[name as keyof T];
